93e48c44b1451ea8a6be044ac4fad18c400cd12e
John Henderson <johnhenderson@fastmail.fm>
2018-02-23 21:24:09 +0000

    Bug fix in controller

--- app/controllers/gadgets_controller.rb
+++ app/controllers/gadgets_controller.rb
@@ -1,12 +1,9 @@
 class GadgetsController < ApplicationController
 
   def index
-    # The strings I stored in database rows 1, 2 & 3 included HTML character codes.
-    # However, the webpage displayed the codes rather than the symbols they represented.
-    # So I had to delete those rows and start again with 4, 5 & 6.
-    @gadget1 = Gadget.find(4)
-    @gadget2 = Gadget.find(5)
-    @gadget3 = Gadget.find(6)
+    @gadget1 = Gadget.find(1)
+    @gadget2 = Gadget.find(2)
+    @gadget3 = Gadget.find(3)
   end
 
 end
