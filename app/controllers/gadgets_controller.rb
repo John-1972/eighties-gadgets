714cf5078bbe013e1e40d9e649d438f61ffdb111
John Henderson <johnhenderson@fastmail.fm>
2018-02-23 19:31:43 +0000

    Added links to photos & background img, some CSS

--- app/controllers/gadgets_controller.rb
+++ app/controllers/gadgets_controller.rb
@@ -1,6 +1,9 @@
 class GadgetsController < ApplicationController
 
   def index
+    # The strings I stored in database rows 1, 2 & 3 included HTML character codes.
+    # However, the webpage displayed the codes rather than the symbols they represented.
+    # So I had to delete those rows and start again with 4, 5 & 6.
     @gadget1 = Gadget.find(4)
     @gadget2 = Gadget.find(5)
     @gadget3 = Gadget.find(6)
