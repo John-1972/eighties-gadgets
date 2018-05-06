2da2390df7ab320b0d90f3beeed5ba481d511e0d
John Henderson <johnhenderson@fastmail.fm>
2018-05-06 20:23:50 +0000

    Moved the new gadget form into a modal

--- app/controllers/gadgets_controller.rb
+++ app/controllers/gadgets_controller.rb
@@ -4,7 +4,14 @@ class GadgetsController < ApplicationController
     @gadgets = Gadget.all
   end
 
-  def new
-    @gadget = Gadget.new
+  def create
+    Gadget.create(gadget_params) # Sends gadget_name & description to be saved in the DB
+    redirect_to root_path # Once info is submitted, take user back to index page
+  end
+
+  private
+
+  def gadget_params # Pulls the values of gadget_name & description from the form
+    params.require(:gadget).permit(:gadget_name, :description)
   end
 end
